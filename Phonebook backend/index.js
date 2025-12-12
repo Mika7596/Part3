4dc8dda67c17ca0b7ff518fe117b11ada7047615
Mika7596 <mika7596@gmail.com>
2025-12-12 16:03:54 +0100

    Configurated port to deploy backend

--- Phonebook backend/index.js
+++ Phonebook backend/index.js
@@ -92,6 +92,6 @@ app.post("/api/persons", (request, response) => {
   response.send(`${newEntry} added to phonebook successfully!`)
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
